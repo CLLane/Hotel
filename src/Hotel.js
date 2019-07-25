19c2eba4bfdd9d044b39a928684826095e0ca899
Chris Lane <pankokrumbs@gmail.com>
2019-07-25 15:17:46 -0600

    Add dot notation to the Hotel class so now the data stored is an array of objects

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,9 +1,9 @@
 class Hotel {
   constructor(customerData, roomData, bookingData, roomServiceData) {
-    this.customerData = customerData;
-    this.roomData = roomData;
-    this.bookingData = bookingData;
-    this.roomServiceData = roomServiceData;
+    this.customerData = customerData.users;
+    this.roomData = roomData.rooms;
+    this.bookingData = bookingData.bookings;
+    this.roomServiceData = roomServiceData.roomServices;
   }
 }
 
